fac43058bb30b9ea80e4b768d937a55694631d20
WeizenMehl <167409829+WeizenMehl@users.noreply.github.com>
2026-08-06 23:39:52 +0200

    made some basic cli stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use clap::Parser;
+use std::io;
+use std::fs::File;
+
+/// And simple CLI based password manager
+#[derive(Parser)]
+struct Cli{
+    /// Initiates the Packages Mangager
+    #[arg(short,long)]
+    init: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    if args.init {
+        match init(){
+            Ok(()) => println!("Initiation was succesfull"),
+            Err(error) => {
+                println!("Couldnt initialize: {}", error)
+            }
+        }
+    }
+
+}
+fn init() -> std::io::Result<()>{
+    println!("Please input MasterPassword!\n(!IMPORTANT! This password cant be reseted if you lose it you lose everthing)");
+    let mut encryption_key = String::new(); //this will for now serve no purpose but eventually encryption will be implemented
+    io::stdin()
+        .read_line(&mut encryption_key)
+        .expect("Error when trying to read password input");
+    File::create("data.csv")?;
+    Ok(())
+
 }
